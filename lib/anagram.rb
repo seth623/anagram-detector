f6114d7d322d5a7d56a4d693376b886425508982
seth <seth623@gmail.com>
2022-01-21 16:06:45 -0700

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word 
+    end
+
+    def match(anagram_array)
+        final_array = []
+
+        split_initialized_word = @word.split("") 
+        sorted_initialized_word = split_initialized_word.sort
+
+        anagram_array.each do |word|
+            split_word = word.split("") 
+            sorted_word = split_word.sort
+            if sorted_initialized_word == sorted_word 
+                final_array << word 
+            end  
+        end
+        
+        return final_array
+
+    end 
+
+end 
